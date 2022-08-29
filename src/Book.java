baa3f9a0d0f962ce660cad4f445a5781c8953f3c
Sergey P <seregapanin23703@gmail.com>
2022-08-29 22:28:31 +0300

    Пробный вариант

--- src/Book.java
+++ src/Book.java
@@ -1,24 +1,27 @@
 public class Book {
-   private String name ;            //поля
+   private String name ;
+   Author author ;     //поля
    private int yearOfPublic;        //поля
 
 
 
-    public Book(String name, int yearOfPublic) { // Конструктор
-        this.name = name;                        //
-        this.yearOfPublic = yearOfPublic;        //
+    public Book(String name,Author author, int yearOfPublic) { // Конструктор
+        this.name = name;
+        this.author = author;//
+        this.yearOfPublic = yearOfPublic;
+        //
     }
 
     public  String getName() {  //Геттер
         return this.name = name ;
     }
 
-    public int getYear() {      //Геттер
-        return this.yearOfPublic;
+    public Author getAuthor() {
+        return this.author;
     }
 
-    public void setName(String name) {      //Сеттер
-        this.name = name;
+    public int getYear() {      //Геттер
+        return this.yearOfPublic;
     }
 
     public void setYearOfPublic(int yearOfPublic) {     //Сеттер
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Hero out time", 1840);
-        book1.getName();
-        System.out.println(book1.getName());
-        book1.setName("Three hundred bucks");
-        System.out.println("book1.getName() = " + book1.getName());
+        Author author1 = new Author("Michail","Lermontov");
+        Book book1 = new Book("Hero out time",author1,1022);
+        Author author2 = new Author("Hemingway","Ernest");
+        Book book2 = new Book("The Old Man and the Sea",author2,1958);
+        book1.setYearOfPublic(232);
+
+        System.out.println(book1.getYear());
+
 
 
     }
