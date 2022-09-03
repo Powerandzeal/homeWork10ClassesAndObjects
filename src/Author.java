a4f1ad58275ac65bcbc61ed3da46f889159b56ef
Sergey P <seregapanin23703@gmail.com>
2022-09-03 19:24:55 +0300

    Сделал ДЗ

--- src/Author.java
+++ src/Author.java
@@ -1,19 +1,42 @@
-public  class Author {
-     private String nameAuthor;         //Поля
-     private String secondNameAuthor;   //
-     public  Author (String nameAuthor, String secondNameAuthor) { // Конструктор
-         this.nameAuthor = nameAuthor;                             //
-         this.secondNameAuthor = secondNameAuthor;                 //
+import java.util.Objects;
 
-     }
+public class Author {
+    private String nameAuthor;         //Поля
+    private String secondNameAuthor;   //
+
+
+    public Author(String nameAuthor, String secondNameAuthor) { // Конструктор
+        this.nameAuthor = nameAuthor;                             //
+        this.secondNameAuthor = secondNameAuthor;                 //
+
+    }
 
     public String getNameAuthor() {  //Сеттер
-         return this.nameAuthor;
+        return this.nameAuthor;
     }
 
     public String getSecondNameAuthor() { //Сеттер
-         return this.secondNameAuthor;
+        return this.secondNameAuthor;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(nameAuthor, author.nameAuthor) && Objects.equals(secondNameAuthor, author.secondNameAuthor);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAuthor, secondNameAuthor);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "nameAuthor='" + nameAuthor + '\'' +
+                ", secondNameAuthor='" + secondNameAuthor + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Author author1 = new Author("Michail","Lermontov");
-        Book book1 = new Book("Hero out time",author1,1022);
-        Author author2 = new Author("Hemingway","Ernest");
-        Book book2 = new Book("The Old Man and the Sea",author2,1958);
-        book1.setYearOfPublic(232);
+        Author author1 = new Author("Michail", "Lermontov");
+        Book book1 = new Book("Hero out time", author1, 1022);
 
-        System.out.println(book1.getYear());
+        Author author2 = new Author("Michail", "Lermontov");
+        Book book2 = new Book("Hero out time", author2, 1022);
+
+
+        System.out.println(book1);
+
+        System.out.println(book1.hashCode() == book2.hashCode());
+        System.out.println(book1.equals(book2));
 
 
 
